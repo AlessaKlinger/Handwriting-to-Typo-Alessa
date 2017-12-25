7b9017d73d6da48f5b057049261de6b5933dc0a7
LAPTOP-7BARVGAJ\aless <alessa.klinger@googlemail.com>
2017-12-25 21:51:30 +0100

    used myscript

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,8 +1,16 @@
 function setup() {
   // put setup code here
-  var string = OCRAD(image);
-alert(string);
-}
+  var editorElement = document.getElementById('editor');
+
+    MyScript.register(editorElement, {
+      recognitionParams: {
+        type: 'MATH',
+        server: {
+          applicationKey: '#YOUR MYSCRIPT CDK APPLICATION KEY#',
+          hmacKey: '#YOUR MYSCRIPT CDK HMAC KEY#'
+        }
+      }
+    });
 
 function draw() {
   // put drawing code here
